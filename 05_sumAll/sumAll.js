749179604bb59ed1d79fbbbe3b5ce3b45081b7ef
Jarod Bernabe <jarodbernabe1100@gmail.com>
2021-10-09 11:38:50 +0800

    Solved first seven exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let finalSum = 0;
 
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  } else if (typeof num1 !== "number" || typeof num2 !== "number") {
+    return "ERROR";
+  }
+
+  // else if above: if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+
+  if (num1 > num2) {
+    for (let i = num1; i >= num2; i--) {
+      finalSum += i;
+    }
+  } else {
+    for (let i = num1; i <= num2; i++) {
+      finalSum += i;
+    }
+  }
+
+  return finalSum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function (fNum) {
+  let result = (fNum - 32) / 1.8;
+  let resultRounded = Math.round(result * 10) / 10;
+  return resultRounded;
 };
 
-const ctof = function() {
-
+const ctof = function (cNum) {
+  let result = cNum * 1.8 + 32;
+  let resultRounded = Math.round(result * 10) / 10;
+  return resultRounded;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
