90e4f187e45fc58d5881b674b1ef3592ba075535
Jarod Bernabe <jarodbernabe1100@gmail.com>
2021-10-26 20:58:17 +0800

    Passed all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let re = /[\W_]/g;
+  let lowercaseStr = string.toLowerCase().replace(re, "");
+  let reverseStr = lowercaseStr.split("").reverse().join("");
 
+  return lowercaseStr === reverseStr;
 };
 
 // Do not edit below this line
