d7c165bdccdc4bbe9a6bbe70f09a01cfbd2448d8
Jarod Bernabe <jarodbernabe1100@gmail.com>
2021-10-26 21:46:51 +0800

    Passed all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (num < 0) {
+    return "OOPS";
+  }
 
+  let a = 0;
+  let b = 1;
+  let c;
+
+  const fibo = [];
+
+  for (let i = 1; i <= num; i++) {
+    fibo.push(b);
+    c = a + b;
+    a = b;
+    b = c;
+  }
+
+  return fibo[fibo.length - 1];
 };
 
 // Do not edit below this line
