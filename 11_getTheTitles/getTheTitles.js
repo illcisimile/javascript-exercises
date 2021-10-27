34112b35a2be5cdb08dcafb7039a645c9409c965
Jarod Bernabe <jarodbernabe1100@gmail.com>
2021-10-27 19:14:27 +0800

    Added solution as comment

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,8 @@
 const getTheTitles = function (array) {
   const bookTitles = [];
 
+  // return array.map((book) => book.title);
+
   for (let i = 0; i < array.length; i++) {
     bookTitles.push(array[i].title);
   }
