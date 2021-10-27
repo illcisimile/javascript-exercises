3831b739f5d60b72754a8ec122d481ba67a6804e
Jarod Bernabe <jarodbernabe1100@gmail.com>
2021-10-27 19:04:24 +0800

    Passed the test

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function (array) {
+  const bookTitles = [];
 
+  for (let i = 0; i < array.length; i++) {
+    bookTitles.push(array[i].title);
+  }
+
+  return bookTitles;
 };
 
 // Do not edit below this line
